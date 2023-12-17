737b4b3d4ac32b2a6c63c7ff1d08e967faac1086
lxyDv <152329352+lxyDv@users.noreply.github.com>
2023-12-17 22:24:02 +0300

    main code added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # alert_checker
+Plan: [done] //use viper for yaml format
+             //Parse data and structure it with length for table
+             //drow table with lost and new detects
+             //export to csv or db
+             //add rule content - interactivily
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strconv"
 
 	"github.com/spf13/viper"
+)
+
+const (
+	//Colors
+	ColorRed    string = "\u001b[31m"
+	ColorGreen         = "\u001b[32m"
+	ColorBlack         = "\u001b[30m"
+	ColorYellow        = "\u001b[33m"
+	ColorBlue          = "\u001b[34m"
+	ColorReset         = "\u001b[0m"
 )
 
 type alert struct {
@@ -20,7 +38,20 @@ type config struct {
 	new_log          string
 }
 
-func parseConfig() {
+func main() {
+	clear_terminal()
+
+	a := alert{file: "44487149_84272ed585db9b05ec46a6691297b49a", timestamp: "07/21/2021-20:11:41.543776", sid: 44487149, msg: "Trojan.Agent.HTTP.C&C"}
+	log.Println(a)
+
+	conf := parseConfig()
+	log.Println(conf)
+
+	export_csv(a)
+
+}
+
+func parseConfig() config {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
 	//viper.AddConfigPath("/etc/appname/")  // path to look for the config file in
@@ -30,63 +61,135 @@ func parseConfig() {
 	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-
-	old_log := viper.GetString("old_log_path")
-	new_log := viper.GetString("new_log_path")
-
-	alert_regexp := viper.GetString("alert_regexp")
-	file_regexp := viper.GetString("file_regexp")
-
-	fmt.Println(alert_regexp)
-	fmt.Println(file_regexp)
-
-	fmt.Println(old_log)
-	fmt.Println(new_log)
-
-	//return config
-
+	conf := config{alert_pattern: viper.GetString("alert_regexp"), filename_pattern: viper.GetString("file_regexp"), old_log: viper.GetString("old_log_path"), new_log: viper.GetString("new_log_path")}
+	return conf
 }
 
-func main() {
+func clear_terminal() {
+	switch runtime.GOOS {
+	case "darwin":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "linux":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
 
-	a := alert{file: "44487149_84272ed585db9b05ec46a6691297b49a", timestamp: "07/21/2021-20:11:41.543776", sid: 44487149, msg: "Trojan.Agent.HTTP.C&C"}
-	fmt.Println(a)
-
-	parseConfig()
-
+	}
 }
 
-//use viper for yaml format
-//structrure with lenght for table
-//export to csv or db
-//add rule content - interactivity
+func colorize(color string, message string) {
+	fmt.Println(string(color), message, string(ColorReset))
+}
 
-//--------------------------------------------------------------CSV PART
-// package main
+func open_file(filePath string) []string {
 
-// import (
-//     "encoding/csv"
-//     "fmt"
-//     "os"
-// )
+	//read file
+	readFile, err := os.Open(filePath)
 
-// func main() {
-//     f, e := os.Create("./People.csv")
-//     if e != nil {
-//         fmt.Println(e)
-//     }
+	// check errors
+	if err != nil {
+		fmt.Println(err)
+	}
 
-//     writer := csv.NewWriter(f)
-//     var data = [][]string{
-//         {"Name", "Age", "Occupation"},
-//         {"Sally", "22", "Nurse"},
-//         {"Joe", "43", "Sportsman"},
-//         {"Louis", "39", "Author"},
-//     }
+	// scan lines from file
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
 
-//     e = writer.WriteAll(data)
-//     if e != nil {
-//         fmt.Println(e)
-//     }
-// }
-//--------------------------------------------------------------CSV PART
+	// save
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+	readFile.Close()
+	return fileLines
+}
+
+func parse_log_slice(log_content []string, r *regexp.Regexp, h *regexp.Regexp) []string {
+
+	var alerts_list []string
+	var filename string
+
+	for _, v := range log_content {
+
+		hash := h.FindStringSubmatch(v)
+		if len(hash) > 0 {
+			filename = hash[1]
+		}
+
+		match := r.FindAllStringSubmatch(v, -1)
+
+		if len(match) > 0 {
+			al := filename + " " + match[0][2] + " " + match[0][1] + " " + match[0][3]
+			//fmt.Printf("%q", al)
+			alerts_list = append(alerts_list, al)
+
+		}
+	}
+	return alerts_list
+}
+
+// func rule_normalize (rulePath, sid string) string{
+func rule_normalize_test(rulePath string) {
+
+	re := regexp.MustCompile(`(?m)\\$\s`)
+	substitution := ""
+
+	//read file
+	readFile, err := os.Open(rulePath)
+
+	// check errors
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// scan lines from file
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	// save
+	var ruleLines []string
+
+	for fileScanner.Scan() {
+		ruleLines = append(ruleLines, fileScanner.Text())
+	}
+	readFile.Close()
+
+	for _, line := range ruleLines {
+		fmt.Println(re.ReplaceAllString(line, substitution))
+
+	}
+}
+
+// do for []alert and pass rule content by sid
+func export_csv(a alert) {
+	f, e := os.Create("./alerts.csv")
+	if e != nil {
+		fmt.Println(e)
+	}
+
+	writer := csv.NewWriter(f)
+	var data = [][]string{
+		{"File", "Sid", "Timestamp", "Rule"},
+		{a.file, strconv.Itoa(a.sid), a.timestamp, a.msg},
+		{a.file, strconv.Itoa(a.sid), a.timestamp, a.msg},
+	}
+
+	e = writer.WriteAll(data)
+	if e != nil {
+		fmt.Println(e)
+	}
+}
+
+//func export_db () {}
+//func draw_table (length width) {}
